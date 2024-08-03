cc4dcd5b5c30045b01db47c209edfdc3c27d4d07
harshdangaich <harsh.dangaich@gmail.com>
2024-08-03 13:50:35 +0530

    Final commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const port = 5000
 const mongoDB=require("./db")
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  // res.setHeader("Access-Control-Allow-Origin","https://food-froontend.onrender.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With,Content-Type,Accept"
